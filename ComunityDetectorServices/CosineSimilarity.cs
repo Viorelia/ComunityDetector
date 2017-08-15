25f8deab89d89c67bf5d63367b21d763e2406530
Viorelia <viorelia.velea@gmail.com>
2017-08-15 16:39:45 +0300

    Remove Idf from Similarity metric + some adjustments for Cosine similarity

--- ComunityDetectorServices/CosineSimilarity.cs
+++ ComunityDetectorServices/CosineSimilarity.cs
@@ -6,20 +6,20 @@ namespace ComunityDetectorServices
 {
     public class CosineSimilarity
     {
-        private Dictionary<string, Dictionary<int, double>> _tfIdf;
+        private Dictionary<string, Dictionary<int, double>> _termFrequency;
         private int _numberDocs;
 
         public CosineSimilarity(List<string> docs)
         {
-            _tfIdf = new Dictionary<string, Dictionary<int, double>>();
+            _termFrequency = new Dictionary<string, Dictionary<int, double>>();
             _numberDocs = docs.Count;
-            BuildTfIdf(docs);
+            BuildTf(docs);
         }
 
         public double GetCosineSimilarityBetweenDocs(int indexDoc1, int indexDoc2)
         {
             double sum = 0;
-            foreach(var term in _tfIdf)
+            foreach(var term in _termFrequency)
             {
                 var docs = term.Value;
                 sum += docs[indexDoc1] * docs[indexDoc2];
@@ -28,43 +28,32 @@ namespace ComunityDetectorServices
             return Math.Acos(sum) * 180 / Math.PI;
         }
 
-        private void BuildTfIdf(List<string> docs)
+        private void BuildTf(List<string> docs)
         {
-            var termsFrequencyList = new List<Dictionary<string, double>>();
-            var termsFrequencyDictionary = new Dictionary<string, Dictionary<int, double>>();
+            var termsFrequencyList = new List<Dictionary<string, int>>();
                   
             foreach (var doc in docs)
             {
                 var tf = GetTermFrequencyVector(doc);
-                var ntf = GetNormalizedTermFrequencyVector(tf);
-                AddMissingTerms(termsFrequencyDictionary, tf);
-                termsFrequencyList.Add(ntf);
+                AddMissingTerms(_termFrequency, tf);
+                termsFrequencyList.Add(tf);
             }
 
-            GlobalizeTerms(termsFrequencyList, termsFrequencyDictionary);
-
-            foreach (var term in termsFrequencyDictionary)
-            {
-                _tfIdf.Add(term.Key, new Dictionary<int, double>());
-                var idf = GetInverseDocumentFrecuency(term.Key, term.Value);
-
-                foreach (var doc in term.Value)
-                {                    
-                    _tfIdf[term.Key].Add(doc.Key, doc.Value * idf);
-                }                
-            }
+            GlobalizeTerms(termsFrequencyList, _termFrequency);
         }      
 
-        private void GlobalizeTerms(List<Dictionary<string, double>> termsFrequencyList, Dictionary<string, Dictionary<int, double>> termsDictionary)
+        private void GlobalizeTerms(List<Dictionary<string, int>> termsFrequencyList, Dictionary<string, Dictionary<int, double>> termsDictionary)
         {
             for(int i=1; i <= _numberDocs; i++)
             {
-                var doc = termsFrequencyList.ElementAt(i-1);
+                var tf = termsFrequencyList.ElementAt(i-1);
+                var ntf = GetNormalizedTermFrequencyVector(tf);
+
                 foreach(var term in termsDictionary.Keys)
                 {
                     double termFrequency;
-                    doc.TryGetValue(term, out termFrequency);
-                    
+                    ntf.TryGetValue(term, out termFrequency);
+                                        
                     termsDictionary[term].Add(i, termFrequency);
                 }
             }
@@ -74,7 +63,7 @@ namespace ComunityDetectorServices
         {
             var words = content.Split(new char[] { ' ', '.', ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
 
-            return words.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+            return words.GroupBy(x => x, StringComparer.OrdinalIgnoreCase).ToDictionary(g => g.Key.ToLowerInvariant(), g => g.Count());
         }
 
         private void AddMissingTerms(Dictionary<string, Dictionary<int, double>> termsDictionary, Dictionary<string, int> terms)
@@ -111,14 +100,6 @@ namespace ComunityDetectorServices
             }
 
             return Math.Sqrt(sum);
-        }
-
-        private double GetInverseDocumentFrecuency(string term, Dictionary<int, double> termsFrequency)
-        {
-            var numberDocsWhereTerm = termsFrequency.Count(x => x.Value > 0);
-            var value = (double)_numberDocs / (1 + numberDocsWhereTerm);
-
-            return Math.Log(value);
         }
     }
 }
