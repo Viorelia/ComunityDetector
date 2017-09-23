339a5ae5d28b76e642cc113b8488b1fbe616a7ed
Viorelia <viorelia.velea@gmail.com>
2017-09-23 21:58:09 +0300

    Created MVC Project and first version of Web crawler

---
+++ ComunityDetector.Web/Controllers/HomePageController.cs
@@ -0,0 +1,17 @@
+﻿using ComunityDetectorServices;
+using System.Web.Mvc;
+
+namespace ComunityDetector.Web.Controllers
+{
+    public class HomePageController : Controller
+    {
+        // GET: HomePage
+        public ActionResult Index()
+        {
+            var webCrawler = new WebCrawler();
+            webCrawler.SearchWeb();
+
+            return View();
+        }
+    }
+}
---
+++ ComunityDetectorServices/WebCrawler.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium.PhantomJS;
+
+namespace ComunityDetectorServices
+{
+    public class WebCrawler
+    {
+        public void SearchWeb()
+        { 
+            var driver = new PhantomJSDriver();
+            driver.Url = "http://ieeexplore.ieee.org/browse/conferences/title/";
+            driver.Navigate();
+            //the driver can now provide you with what you need (it will execute the script)
+            //get the source of the page
+            var source = driver.PageSource;
+            //fully navigate the dom
+            //var pathElement = driver.FindElementById("some-id");
+
+            System.IO.File.WriteAllText(@"D:\Work\Dizertatie\Aplicatia\results.txt", source);
+        }
+    }
+}
